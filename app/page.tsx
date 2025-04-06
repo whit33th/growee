b4050276d62d3e3b2da1414bf74db4ff9529488f
Daniil Kaminskyi <deniil019@gmail.com>
2025-04-06 23:47:28 +0200

    added img remote domain

--- app/page.tsx
+++ app/page.tsx
@@ -18,7 +18,7 @@ export default async function Home() {
       userId: userId,
     },
     orderBy: {
-      id: "desc",
+      nextWaterDate: "asc",
     },
   });
 
--- next.config.ts
+++ next.config.ts
@@ -12,6 +12,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "img.clerk.com",
       },
+      {
+        protocol: "https",
+        hostname: "tan-effective-kite-240.mypinata.cloud",
+      },
     ],
   },
 };
